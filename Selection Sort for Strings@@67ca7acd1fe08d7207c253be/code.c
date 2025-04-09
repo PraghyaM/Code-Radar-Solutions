0d49d95241bc1ed67eb77e1d0a0913edcba320b2
Praghya <praghyamahajan2006@gmail.com>
2025-04-10 01:45:59 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,21 +1,51 @@
-void selectionSort(char arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
+#include <stdio.h>
+#include <string.h>
+
+// Function to perform selection sort
+void selectionSort(char arr[][100], int n) {
+    int i, j, min_idx;
+    char temp[100];
+    
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (strcmp(arr[j], arr[min_idx]) < 0) {
                 min_idx = j;
             }
         }
         // Swap arr[i] and arr[min_idx]
-        char temp = arr[i];
-        arr[i] = arr[min_idx];
-        arr[min_idx] = temp;
+        if (min_idx != i) {
+            strcpy(temp, arr[i]);
+            strcpy(arr[i], arr[min_idx]);
+            strcpy(arr[min_idx], temp);
+        }
     }
 }
 
-void printArray(char arr[], int n) {
+// Function to print the array
+void printArray(char arr[][100], int n) {
     for (int i = 0; i < n; i++) {
-        printf("%c ", arr[i]);
+        printf("%s\n", arr[i]);
     }
-    printf("\n");
 }
+
+int main() {
+    int n;
+    
+    // Read the number of strings
+    scanf("%d", &n);
+    char arr[n][100];  // Array of strings with max length 100
+
+    // Read each string from input
+    for (int i = 0; i < n; i++) {
+        scanf("%s", arr[i]);
+    }
+
+    // Call the selectionSort function
+    selectionSort(arr, n);
+
+    // Print the sorted array of strings
+    printArray(arr, n);
+    return 0;
+}
+
